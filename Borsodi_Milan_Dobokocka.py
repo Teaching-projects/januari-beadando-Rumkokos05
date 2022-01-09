96602994af252c886957ed0bb34bc1bd1440e144
Rumkokos05 <borsodi.milan05@gmail.com>
2022-01-09 20:22:08 +0100

    magyarázó változók

--- Borsodi_Milan_Dobokocka.py
+++ Borsodi_Milan_Dobokocka.py
@@ -1,18 +1,18 @@
 import random
 szam=int(input("Add meg mekkora a legnyagyobb dobható szám: "))
 print()
-i=1
-f=1
-while i == 1:
+valasz=1
+ujraker=1
+while valasz == 1:
     randomszam = random.randint(1, szam)
     print("A számod:", randomszam)
     print()
-    f=1
-    while f == 1:
+    ujraker=1
+    while ujraker == 1:
         ujra=input("Újra szeretnél dobni? Y/N: ")
         if ujra == "N" or ujra == "n":
-            i=0
-            f=0
+            valasz=0
+            ujraker=0
             print("Program leáll")
         if ujra == "Y" or ujra == "y":
-            f=0
+            ujraker=0
